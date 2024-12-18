a0ef8dac7c3d385b940bf704a57aa41a8b1acb6a
alturismo <alturismo@gmail.com>
2024-12-18 07:36:07 +0100

    update logging

--- src/usr/local/emhttp/plugins/pwm-fan/scripts/pwm_logs.sh
+++ src/usr/local/emhttp/plugins/pwm-fan/scripts/pwm_logs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /var/log/syslog | grep -i "pwm.*fan:" >> /usr/local/emhttp/plugins/pwm-fan/pwm_log.txt
+cat /var/log/syslog | grep -i "pwm.*fan:" > /usr/local/emhttp/plugins/pwm-fan/pwm_log.txt
