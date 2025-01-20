c270bb658ea3bc4ee68764030eccd53479281095
alturismo <alturismo@gmail.com>
2025-01-20 19:01:46 +0100

    fix

--- src/usr/local/emhttp/plugins/pwm-fan/scripts/cpu_cron.sh
+++ src/usr/local/emhttp/plugins/pwm-fan/scripts/cpu_cron.sh
@@ -4,7 +4,7 @@ source /boot/config/plugins/pwm-fan/pwmfansettings
 
 if [ ! -z "$cron_time" ]; then
 ( crontab -l | grep -v -F "CPU" ; echo "# Cron Job for CPU pwm-fan plugin" ) | crontab -
-( crontab -l | grep -v -F "pwm-fan.sh" ; echo "$cron_time /usr/local/emhttp/plugins/pwm-fan/scripts/pwm-fan.sh" ) | crontab -
+( crontab -l | grep -v -F "pwm-fan.sh" ; echo "$cron_time /usr/local/emhttp/plugins/pwm-fan/scripts/pwm-fan.sh >/dev/null 2>&1" ) | crontab -
 sed -i 's/^cron_state=.*/cron_state="started"/' /boot/config/plugins/pwm-fan/pwmfansettings
 else
 crontab -l | grep -v "CPU"  | crontab -
--- src/usr/local/emhttp/plugins/pwm-fan/scripts/hdd_cron.sh
+++ src/usr/local/emhttp/plugins/pwm-fan/scripts/hdd_cron.sh
@@ -4,7 +4,7 @@ source /boot/config/plugins/pwm-fan/pwmfansettings
 
 if [ ! -z "$hdd_cron_time" ]; then
 ( crontab -l | grep -v -F "HDD " ; echo "# Cron Job for HDD pwm-fan plugin" ) | crontab -
-( crontab -l | grep -v -F "pwm_hdd.sh" ; echo "$hdd_cron_time /usr/local/emhttp/plugins/pwm-fan/scripts/pwm_hdd.sh" ) | crontab -
+( crontab -l | grep -v -F "pwm_hdd.sh" ; echo "$hdd_cron_time /usr/local/emhttp/plugins/pwm-fan/scripts/pwm_hdd.sh >/dev/null 2>&1" ) | crontab -
 sed -i 's/^hdd_cron_state=.*/hdd_cron_state="started"/' /boot/config/plugins/pwm-fan/pwmfansettings
 else
 crontab -l | grep -v "HDD "  | crontab -
